ce1136748c7eb2a5e96c391a131c1bef3a256a85
Marcus Baguley <marcus.baguley@gmail.com>
2024-05-29 11:31:23 +1200

    remove duplicated login

--- zsh-aliases/alias.zsh
+++ zsh-aliases/alias.zsh
@@ -13,13 +13,6 @@ image-tag () {
   fi
 }
 alias groot='cd $(git rev-parse --show-toplevel)'
-aws-login () {
-  if [[ $1 == 'kiwibank' ]]; then
-    aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 558833745501.dkr.ecr.ap-southeast-2.amazonaws.com
-  else
-    echo 'please pass profile name or configure script'
-  fi
-}
 
 git-diff-untracked () {
   for next in $( git ls-files --others --exclude-standard ) ; do git --no-pager diff --no-index /dev/null $next; done;
